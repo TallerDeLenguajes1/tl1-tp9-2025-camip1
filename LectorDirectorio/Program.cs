d38ea5ac4879f5092f48834bced75da94a2578a8
Perez Milagros Camila <camilap11517@gmail.com>
2025-06-10 12:27:18 -0300

    tercer commit: cargar path de un directorio

---
+++ LectorDirectorio/Program.cs
@@ -0,0 +1,16 @@
+﻿string? input;
+
+do
+{
+    Console.WriteLine("Ingrese la ruta del Directorio que desea analizar:");
+    input = Console.ReadLine();
+    if (!Directory.Exists(input))
+    {
+        Console.WriteLine("Inavlido: Directorio inexistente. Intente nuevamente.");
+    }
+} while (!Directory.Exists(input));
+
+Console.WriteLine("---------------");
+Console.WriteLine("Listado de carpetas");
+
+
