51ee39de85361bd4d27b34c92c9bd43e4fa1f084
Perez Milagros Camila <camilap11517@gmail.com>
2025-06-10 13:17:28 -0300

    cuarto commit: mostrar carpetas y archivos con tamaño

--- LectorDirectorio/Program.cs
+++ LectorDirectorio/Program.cs
@@ -1,16 +1,37 @@
-﻿string? input;
+﻿string? Directorio;
 
 do
 {
     Console.WriteLine("Ingrese la ruta del Directorio que desea analizar:");
-    input = Console.ReadLine();
-    if (!Directory.Exists(input))
+    Directorio = Console.ReadLine();
+    if (!Directory.Exists(Directorio))
     {
         Console.WriteLine("Inavlido: Directorio inexistente. Intente nuevamente.");
     }
-} while (!Directory.Exists(input));
+} while (!Directory.Exists(Directorio));
 
-Console.WriteLine("---------------");
-Console.WriteLine("Listado de carpetas");
+List<string> ListadoCarpetas = Directory.GetDirectories(Directorio).ToList();
+List<string> ListadoArchivos = Directory.GetFiles(Directorio).ToList();
+
+Console.WriteLine("---------------------");
+Console.WriteLine("Listado de carpetas en \"" + Directorio + "\"");
+
+foreach (string Carpeta in ListadoCarpetas)
+{
+    Console.WriteLine(Carpeta);
+}
+
+Console.WriteLine("---------------------");
+Console.WriteLine("Listado de archivos en \"" + Directorio + "\"");
+
+foreach (string Archivo in ListadoArchivos)
+{
+    if (Archivo != ".gitignore")
+    {
+        Console.WriteLine(Archivo);
+        FileInfo fileOp = new FileInfo(Archivo);
+        Console.WriteLine("Tamaño: {0} KB" , ((fileOp.Length)/1024));
+    }
+}
 
 
